4f72032682c1d19d8432d97d296d763e058092d6
ongddree <ongddree@gmail.com>
2021-12-19 22:22:35 +0900

    :memo: routes-setting

---
+++ src/pages/wines/dessert.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Dessert: NextPage = () => {
+  return (
+    <div>
+      <h1>Wine</h1>
+    </div>
+  );
+};
+
+export default Dessert;
---
+++ src/pages/wines/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Winespage: NextPage = () => {
+  return (
+    <div>
+      <h1>Wines</h1>
+    </div>
+  );
+};
+
+export default Winespage;
