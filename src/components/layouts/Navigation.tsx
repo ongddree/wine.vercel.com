da155746096d8a0184b16d99a1289c5d9955c65e
ongddree <ongddree@gmail.com>
2021-12-23 23:56:30 +0900

    :bug: bug: SUBS key error

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -12,7 +12,33 @@ export const Navigation = () => {
   return (
     <header>
       <h1>Wine and Beers</h1>
-      <nav></nav>
+      <nav>
+        <ul>
+          {ROUTES.map((routeObject: ROUTE) => {
+            return (
+              <li>
+                <Link href={routeObject.PATH}>
+                  <a>{routeObject.LABEL}</a>
+                </Link>
+                <ul>
+                  {routeObject.SUBS &&
+                    routeObject.SUBS.map((subRouteObject: ROUTE) => {
+                      return (
+                        <li>
+                          <Link
+                            href={`${routeObject.PATH}${subRouteObject.PATH}`}
+                          >
+                            <a>{subRouteObject.LABEL}</a>
+                          </Link>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </header>
   );
 };
